8cecb5899cc9d42133cf4331f56d549899fafa41
Yannis Haismann <72089971+YannisHaismann@users.noreply.github.com>
2021-09-09 09:35:02 +0200

    form my profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Campus;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,8 +13,14 @@ class UserController extends AbstractController
 
     public function index(): Response
     {
+        $user = $this->getUser();
+
+        $campus = $this->getDoctrine()->getRepository(Campus::class)->findAll();
+
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+            'user' => $user,
+            'campus' => $campus,
         ]);
     }
 
